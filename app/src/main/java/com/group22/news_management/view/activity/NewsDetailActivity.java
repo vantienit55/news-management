243e804f7c55f6f4eac091ebfd206e6ff6e074bf
vantienit55 <19522331@gm.uit.edu.vn>
2021-12-18 08:45:06 +0700

    sua

--- app/src/main/java/com/group22/news_management/view/activity/NewsDetailActivity.java
+++ app/src/main/java/com/group22/news_management/view/activity/NewsDetailActivity.java
@@ -1,16 +1,27 @@
 package com.group22.news_management.view.activity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.group22.news_management.utils.HTMLParser;
 import com.group22.newsmanagerment.R;
 
+import java.util.ArrayList;
+
 public class NewsDetailActivity extends AppCompatActivity {
 
     private BottomAppBar bottomAppBar;
+    private WebView webView1;
+    private Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +60,19 @@ public class NewsDetailActivity extends AppCompatActivity {
                 }
                 case R.id.icon_font_size:{
                     Toast.makeText(NewsDetailActivity.this, "icon_font_size", Toast.LENGTH_SHORT).show();
+                   /* webView1 = (WebView) findViewById(R.id.webView);
+                    spinner1 = (Spinner) findViewById(R.id.icon_font_size);
+                    ArrayList<String> arraySize = new ArrayList<String>();
+                    arraySize.add("To");
+                    arraySize.add("Vua");
+                    arraySize.add("Nho");
+                    ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, arraySize);
+                    spinner1.setAdapter(arrayAdapter);
+                    final WebSettings webSettings = webView1.getSettings();
+                    //webSettings.setTextSize(WebSettings.TextSize.SMALLEST);
+                    Resources res = getResources();
+                    float fontSize = res.getInteger(R.integer.size_To);
+                    webSettings.setDefaultFontSize((int) fontSize);*/
                     break;
                 }
             }
